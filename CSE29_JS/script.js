69dabc4deb5938065639e5016eddce66e6dee948
Rajatpal38252 <rajatpal38252@gmail.com>
2026-02-26 22:09:19 +0530

    Checking dat value types

--- CSE29_JS/script.js
+++ CSE29_JS/script.js
@@ -1,27 +1,25 @@
 console.log("Hello World");
-document.write("Hello World");
-var num=10;
-console.log(num);
-console.log(typeof(num));
-num=false;
-console.log(num);
-console.log(typeof(num));
-var num=null;
-console.log(num);
-console.log(typeof(num));
-num=false;
-console.log(num);
-console.log(typeof(num));
+// document.write("Hello World");
 
 
+// Primitive types
 
+var num = 10;
+console.log(num);   // 10
+console.log(typeof num);  // number
 
-const sym1= Symbol(4)
-console.log(sym1);
-const sym2=Symbol(4);
-console.log(sym2);
-if(sym1==sym2){
-    console.log("true");
-}else{
-    console.log("false")//because each symbol is Unique
-}
+var num = "Hello";
+console.log(num);
+console.log(typeof num); //string
+
+var num = true;
+console.log(num);
+console.log(typeof num);  // boolean
+
+var num = null;
+console.log(num);
+console.log(typeof num); // null
+
+var num = undefined;
+console.log(num);
+console.log(typeof num); //undefined
