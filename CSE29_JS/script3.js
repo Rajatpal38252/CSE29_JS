0ef883fcea944b2577b71dc844543db2d865b619
Rajatpal38252 <rajatpal38252@gmail.com>
2026-02-28 17:11:14 +0530

    Assigmment operators

---
+++ CSE29_JS/script3.js
@@ -0,0 +1,36 @@
+var a=100 , b=150 , linebreak="<br>";
+
+document.write("Valueof a=>(a=b)=>");
+result=(a=b);
+document.write(result);
+document.write(linebreak);
+
+document.write("Valueof a=>(a+=b)=>");
+result=(a+=b);
+document.write(result);
+document.write(linebreak);
+
+document.write("Valueof a=>(a-=b)=>");
+result=(a-=b);
+document.write(result);
+document.write(linebreak);
+
+document.write("Valueof a=>(a*=b)=>");
+result=(a*=b);
+document.write(result);
+document.write(linebreak);
+
+document.write("Valueof a=>(a/=b)=>");
+result=(a/=b);
+document.write(result);
+document.write(linebreak);
+
+document.write("Valueof a=>(a%=b)=>");
+result=(a%=b);
+document.write(result);
+document.write(linebreak);
+
+//logical operators//
+// var age=20;
+// var Voteable=(age<18)?"too oyung":"old enough";
+// document.write(Voteable);
