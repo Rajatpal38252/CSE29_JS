759226a2a9494aa1dffd287d564526cc181ee062
Rajatpal38252 <rajatpal38252@gmail.com>
2026-03-11 14:45:41 +0530

    Function

---
+++ CSE29_JS/script4.js
@@ -0,0 +1,70 @@
+// var count;
+// document.write("Starting loop"+"<br/>");
+// for(count=0;count<10;count++){
+//     document.write("Current count:"+count);
+//     document.write("<br/>");
+// }
+//  document.write("loop stopped!");
+
+// const person = {
+//     fname: "Ajay",
+//     lname: "Singh",
+//     age: 25
+// };
+
+// for (let x in person) {
+//     console.log("person details: " + x + ":" + person[x]);
+// }
+
+
+// var age=20;
+
+// if(age>18){
+//     document.write("<b>You are eligible to cast the vote</b>")
+// }
+
+// var grade='A';
+// document.write("Entering switch block<br/>");
+// switch(grade){
+//     case 'A':document.write("Good job<br/");
+//     break;
+
+//     case 'B':document.write("Pretty good<br/>");
+//     break;
+
+//     case 'C':document.write("Passed<br/>");
+//     break;
+
+//     default:document.write("Unknown grade<br/>")
+// }
+
+
+// function myFunction(){
+// alert("Hello World")
+// }
+
+// function sayHello(name , age){
+//     document.write(name+is+age+years old)
+// }
+
+// var add2=(a,b)=>{
+//     console.log(a+b);
+// }
+
+// add2(5,6);
+
+// object is collection of anything
+// represented by curlly bracket
+
+// var person={
+//     name:'John',
+//     age:30,
+//     isMarried:true,
+//     address:{
+//         street:'Vinay Nagar',
+//         flatNO:201
+//     }
+// };
+// console.log(person);
+
+
