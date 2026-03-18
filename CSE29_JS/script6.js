18258510765221b66f1f950356ab47a79c1d4805
Rajatpal38252 <rajatpal38252@gmail.com>
2026-03-18 13:59:44 +0530

    DOM

---
+++ CSE29_JS/script6.js
@@ -0,0 +1,23 @@
+// javascript can change all the html elements in the page
+//javascript can change all the html attributes in the page 
+//javascrript can change all the css styles in the page
+// javascript can removee existing html elemments and attributes 
+//javascript can add new html elements and attributes.
+//javascript can react to all existing html events in the page
+
+
+
+// const x =document.getElementById("p1");
+
+// // console.log(x);
+// x.innerHTML="<b>Data structure</b>";
+
+// const result = document.getElementsByClassName("p1");
+
+// console.log(result[0]); 
+// result[0].innerHTML = "Hello";
+
+    const x=document.getElementById("p2").getAttribute("class");
+    document.getElementById("d").innerHTML=x;
+        
+
